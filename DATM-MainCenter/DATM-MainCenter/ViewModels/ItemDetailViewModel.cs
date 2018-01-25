a3b455e956dccaf45f12a8483a26299611e35a7b
Jan-Willem de Bruyn <jw.debruyn@bsa-site.nl>
2018-01-25 09:33:27 +0100

    Add project files.

---
+++ DATM-MainCenter/DATM-MainCenter/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using DATM_MainCenter.Models;
+
+namespace DATM_MainCenter.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ DATM-MainCenter/DATM-MainCenter/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using DATM_MainCenter.Models;
+using DATM_MainCenter.ViewModels;
+
+namespace DATM_MainCenter.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ItemDetailPage : ContentPage
+	{
+        ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+
+            var item = new Item
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemDetailViewModel(item);
+            BindingContext = viewModel;
+        }
+    }
+}
